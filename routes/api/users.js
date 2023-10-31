c666295d00340836275ccea1baa146999823014e
Nicole Bogart <nicolemariebogart@gmail.com>
2023-10-30 20:17:25 -0600

    fixed issue with importing check

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,15 +1,23 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 const router = express.Router();
-import { check } from "express-validator";
 
 // @route  POST api/users
 // Register user
 // @access Public
 router.post(
   "/",
-  [check("name", "Name is required").not().isEmpty()],
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email address").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 8 or more characters"
+    ).isLength({ min: 6 }),
+  ],
   (req, res) => {
-    console.log(req.body);
+    const errors = validationResult(req);
+
     res.send("User route");
   }
 );
