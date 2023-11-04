b42ac62b58575baabbbee6fe06749cc3a44a14ac
Nicole Bogart <nicolemariebogart@gmail.com>
2023-11-04 08:57:57 -0600

    Changed 'user' to 'current_user' in profile.js, now able to view all users

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const User = require("../../models/User");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 // @route  GET api/auth
 // @access Public
@@ -14,5 +18,54 @@ router.get("/", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// Authenticate user & get token
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email address").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+      // Return token
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,80 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+const { check, validationResult } = require("express-validator");
 
-// @route  GET api/profile
-// @access Public
-router.get('/', (req, res) => res.send('Profile route')); 
+// Get user profile
+router.get("/current_user", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name"]
+    );
+    if (!profile) {
+      return res.status(400).json({ msg: "No profile exists for this user" });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
+// Create or edit user profile
+router.post("/current_user", [auth], async (req, res) => {
+  const { location, bio } = req.body;
+
+  // Build profile
+  const profileFields = {};
+  profileFields.user = req.user.id;
+  if (location) profileFields.location = location;
+  if (bio) profileFields.bio = bio;
+
+  try {
+    let profile = await Profile.findOne({ user: req.user.id });
+    if (profile) {
+      // Edit profile if profile exists
+      profile = await Profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set: profileFields },
+        { new: true }
+      );
+      return res.json(profile);
+    }
+    // Create profile
+    profile = new Profile(profileFields);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// Get all profiles
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name"]);
+    res.json(profiles);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// View specific profile by user id
+// Work in progress
+/*
+router.get("/", async (req, res) => {
+    try {
+      const profiles = await Profile.find().populate("user", ["name"]);
+      res.json(profiles);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  });
+  */
 module.exports = router;
